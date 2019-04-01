7e4cc5b457301ea85601b0f48181555e4d3f1b53
Cooper Strahan <cooperstrahan@Coopers-MacBook-Pro.local>
2019-04-01 12:48:42 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> boxList = new List<object>();
+            boxList.Add(7);
+            boxList.Add(28);
+            boxList.Add(-1);
+            boxList.Add(true);
+            boxList.Add("chair");
+
+            int sum = 0;
+
+            foreach(var item in boxList){
+                if(item is int){
+                    int IsInt = (int)item;
+                    sum += IsInt;
+                }
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Sum: "+sum);
+        }
+    }
+}
